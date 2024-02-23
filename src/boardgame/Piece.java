ee97dfea97adbae8d160b966c526086e86d1b9eb
Vinicius Kalach <viniciuscato@hotmail.com>
2024-02-23 16:07:50 -0300

    Start add possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	
 	protected Position position;
 	private Board board;
@@ -13,6 +13,28 @@ public class Piece {
 	protected Board getBoard() {
 		return board;
 	}
-
 	
+	// Método que retorna movimentos posssiveis representados por uma matriz bidimensional de booleanos.
+	// A implementação específica deve ser fornecida por classes que herdam esta classe abstrata.
+	public abstract boolean[][] possibleMoves();
+	
+	// Método concreto que utiliza o método abstrato (Hook methods)
+	// Esse método valida se há algum movimento possivel em tal posição
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	// '' (Hook methods)
+	// Esse método retorna true ou false conforme a possibilidade de movimento
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i < mat.length; i++) {
+			for (int j=0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
